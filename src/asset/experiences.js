ad20a81d0201fa8814f4c346423f217eb1689a53
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-03-18 00:06:58 +0300

    Experience component created

---
+++ src/asset/experiences.js
@@ -0,0 +1,23 @@
+export const experiences = [
+  {
+    companyName: "Google",
+    title: "Backend Developer",
+    jobDescription: "I am developing backend",
+    startDate: "2019",
+    endDate: "present",
+  },
+  {
+    companyName: "Facebook",
+    title: "Fullstack Developer",
+    jobDescription: "I was working on Facebook's internal platform",
+    startDate: "2018",
+    endDate: "2019",
+  },
+  {
+    companyName: "X Company",
+    title: "Intern",
+    jobDescription: "I was intern in this company",
+    startDate: "2017",
+    endDate: "2018",
+  },
+];
